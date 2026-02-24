cc90419964b2af2a24242210d65d777fc9552e42
adavidys <zazadavidzaza2281337@gmail.com>
2026-02-24 21:53:30 +0200

    Init

---
+++ src/main/java/com/example/turniring/TurniringApplication.java
@@ -0,0 +1,11 @@
+package com.example.turniring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurniringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TurniringApplication.class, args);
+	}
+}
